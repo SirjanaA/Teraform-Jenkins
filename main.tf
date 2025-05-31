ed33f949f455c68def9869fdbdd7ca20e9c109e8
SirjanaA <alesrizu92@icloud.com>
2025-06-01 00:23:46 +0100

    modified main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 
 resource "aws_instance" "tf-instance" {
   ami           = "ami-04d32dab8ea739477"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
 
   tags = {
     Name = "${var.project}-vm"
