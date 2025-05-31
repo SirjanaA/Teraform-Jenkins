2341a5eba177d135f13a1f49a00c1d6230bde64a
SirjanaA <alesrizu92@icloud.com>
2025-05-31 23:31:35 +0100

    modified output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "public_ip_address" {
-    value = [for instance in aws_instance.vm : instance.public_ip]
+    value = aws_instance.tf-instance.public_ip
     description = "The public IP address of the server"
 }
